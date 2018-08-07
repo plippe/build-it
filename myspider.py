a9723ca069cc528c1755263c73d691fba9425ca6
Philippe Vinchon <p.vinchon@gmail.com>
2018-08-07 14:17:29 +0100

    Filter out duplicated items

--- myspider.py
+++ myspider.py
@@ -1,5 +1,6 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from scrapy.exceptions import DropItem
 
 class WiproDigitalSpider(scrapy.Spider):
     name = 'wiprodigital'
@@ -14,10 +15,25 @@ class WiproDigitalSpider(scrapy.Spider):
             yield { 'url': response.request.url, 'anchor': anchor.css('::attr(href)').extract_first() }
             yield response.follow(anchor, self.parse)
 
+class DuplicatesPipeline(object):
+    def __init__(self):
+        self.data = set()
+
+    def process_item(self, item, spider):
+        row = item.get('url') + item.get('image', '') + item.get('anchor', '')
+        if row in self.data:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.data.add(row)
+            return item
+
 if __name__ == "__main__":
     process = CrawlerProcess({
         'FEED_FORMAT': 'jsonlines',
-        'FEED_URI': 'log/wiprodigital.json'
+        'FEED_URI': 'log/wiprodigital.json',
+        'ITEM_PIPELINES': {
+            'myspider.DuplicatesPipeline': 1
+        }
     })
 
     process.crawl(WiproDigitalSpider)
